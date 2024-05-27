b3909bb698a53b830a625b90ae4b11e72f4c1b3d
AsssaduhAwan <astudentsgithub@gmail.com>
2024-05-27 14:56:11 +0500

    Add new project

---
+++ assad.js
@@ -0,0 +1,14 @@
+var istatus = document.querySelector("h3")
+var addFriend = document.querySelector("#add")
+addFriend.addEventListener("click",function (){
+   istatus.innerHTML="Friends"
+  istatus.style.color="green"
+})
+var  ustatus = document.querySelector("h3")
+var removeFriend=document.querySelector("#remove")
+removeFriend.addEventListener("click",function () {
+ustatus.innerHTML=" Birthday 1 August Chocolate Lover "
+ustatus.style.color="black"
+})
+
+
